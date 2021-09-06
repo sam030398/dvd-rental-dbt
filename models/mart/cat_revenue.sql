8fce03afa0e4bead835384b2506b510bb3913d66
samdaniel@impersuasion.com <samdaniel@impersuasion.com>
2021-09-06 00:37:01 +0000

    cat_revenue

---
+++ models/mart/cat_revenue.sql
@@ -0,0 +1,7 @@
+SELECT category_name , category_id, 
+SUM(amount) as revenue , 
+COUNT (DISTINCT(rdeno.rental_id)) as orders_count,
+DATE_DIFF (current_date,max(rental_date), day) as recency
+FROM  {{ ref('rentals_denormalized') }} rdeno , unnest (payments) as py
+GROUP BY 1,2
+ORDER BY 3 DESC

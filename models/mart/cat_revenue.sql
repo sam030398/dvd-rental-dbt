6370a07abaae94cff86c166d723e2c609050163f
samdaniel@impersuasion.com <samdaniel@impersuasion.com>
2021-09-06 01:00:31 +0000

    commit_last

--- models/mart/cat_revenue.sql
+++ models/mart/cat_revenue.sql
@@ -2,7 +2,7 @@ SELECT category_name , category_id,
 SUM(amount) as revenue , 
 COUNT (DISTINCT(rdeno.rental_id)) as orders_count,
 DATE_DIFF (current_date,max(rental_date), day) as recency,
-COUNT(DISTINCT (case when return_status = 'Late Return' THEN rental_id END)) as late_return_count
+COUNT(DISTINCT (case when return_status = 'Late Return' THEN rdeno.rental_id END)) as late_return_count
 FROM  {{ ref('rentals_denormalized') }} rdeno , unnest (payments) as py
 GROUP BY 1,2
 ORDER BY 3 DESC
